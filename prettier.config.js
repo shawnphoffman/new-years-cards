e810c8c825be4cf79ffd079f20d1fb2af7466852
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-01-14 08:06:53 -0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+export default {
+	printWidth: 140,
+	useTabs: true,
+	semi: false,
+	arrowParens: 'avoid',
+	trailingComma: 'es5',
+	singleQuote: true,
+}

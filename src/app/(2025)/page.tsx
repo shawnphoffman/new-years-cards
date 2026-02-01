c5080e59f0c105f929da25b0fae30b637a056fd3
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-02-01 12:46:03 -0800

    Refactor 2025 page layout by removing unnecessary prose classes, enhancing SuperImage component with full width styling, and adding temporary content to card2.mdx for improved structure and clarity.

--- src/app/(2025)/page.tsx
+++ src/app/(2025)/page.tsx
@@ -12,7 +12,7 @@ export default function TwentyFive() {
 		<article className="flex flex-col gap-10 sm:gap-12 lg:gap-20 xl:gap-24 pt-28">
 			<h1 className="text-4xl font-bold">2025 In Review</h1>
 			{/* <div className="prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto"> */}
-			<div className=" mx-auto">
+			<div className="mx-auto">
 				<section>
 					<SidenoteProvider>
 						<Card />
--- src/components/SuperImage.tsx
+++ src/components/SuperImage.tsx
@@ -82,7 +82,7 @@ export default async function SuperImage({ children, src, ...rest }: Props) {
 			{...motionProps}
 		>
 			<div className="image-bg">
-				<Image src={src} {...rest} className={cn('not-prose', rest.className)} alt={alt} />
+				<Image src={src} {...rest} className={cn('not-prose w-full', rest.className)} alt={alt} />
 			</div>
 			{children && (
 				<motion.figcaption variants={captionVariants} {...captionMotionProps}>

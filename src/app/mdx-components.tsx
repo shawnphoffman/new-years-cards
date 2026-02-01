7d324f3d82ac70d0db5ef60367f64a91f9e65bef
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-02-01 12:54:28 -0800

    Refactor mdx-components and card.mdx by commenting out unused animation variants, removing unnecessary image imports, and restructuring content for clarity. Update card2.mdx to include card images and videos with improved organization.

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -17,10 +17,10 @@ export function useMDXComponents(): MDXComponents {
 		img: props => <Image sizes="100vw" style={{ width: '100%', height: 'auto' }} {...(props as ImageProps)} alt="" />,
 		p: props => (
 			<InView
-				variants={{
-					hidden: { opacity: 0, y: 100, filter: 'blur(4px)' },
-					visible: { opacity: 1, y: 0, filter: 'blur(0px)' },
-				}}
+				// variants={{
+				// hidden: { opacity: 0, y: 100, filter: 'blur(4px)' },
+				// visible: { opacity: 1, y: 0, filter: 'blur(0px)' },
+				// }}
 				viewOptions={{ margin: '0px 0px -200px 0px' }}
 				transition={{ duration: 0.3, ease: 'easeInOut' }}
 				once={true}
